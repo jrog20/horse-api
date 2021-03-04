dd43062d2bbbae731adfe90b758274172598f979
jrog20 <jaimerogstad@gmail.com>
2021-03-04 17:21:16 -0500

    Build horses class

--- frontend/src/components/horses.js
+++ frontend/src/components/horses.js
@@ -1,5 +1,14 @@
 class Horses {
   constructor() {
     this.horses = []
+    this.adapter = new HorsesAdapter()
+    // this.bindEventListeners()
+    this.fetchAndLoadHorses()
+  }
+
+  fetchAndLoadHorses() {
+    this.adapter.getHorses().then(horses => {
+      console.log(horses)
+    })
   }
 }
