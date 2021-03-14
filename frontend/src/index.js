57b191a74fed7140235624237083c28796ba6090
jrog20 <jaimerogstad@gmail.com>
2021-03-14 17:28:40 -0400

    Revert to original

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,2 @@
 // Create new instance of App
-// const app = new App()
-
-
-const url = 'http://localhost:3000/api/v1/horses'
-const horsesContainer = document.getElementById('horses-container')
-
-fetch(url)
-.then(function(res) {
-  return res.json()
-}).then(function(horses){
-  for(let horse of horses) {
-    let li = document.createElement('li')
-    li.innerText = horse.barn_name
-    horsesContainer.appendChild(li)
-  }
-})
+const app = new App()
