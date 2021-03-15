924876f79ab8b3d06f5e60cf07f63b5ccd84e2b5
jrog20 <jaimerogstad@gmail.com>
2021-03-15 16:36:31 -0400

    Work on rendering more than one horse

--- frontend/src/components/horses.js
+++ frontend/src/components/horses.js
@@ -8,6 +8,7 @@ class Horses {
 
   initBindingsAndEventListeners() {
     this.horsesContainer = document.getElementById('horses-container')
+    // this.horsesContent = document.getElementsByClassName('horses-content')
     this.photo = document.getElementById('photo')
     this.barnName = document.getElementById('barn-name')
     this.registeredName = document.getElementById('registered-name')
@@ -19,7 +20,7 @@ class Horses {
     this.patternType = document.getElementById('pattern-type')
     this.tested = document.getElementById('tested')
     this.tobianoGene = document.getElementById('tobiano-gene')
-    this.offspring = document.getElementById('offspring')
+    this.offsprings = document.getElementById('offsprings')
 
     // this.newBarnName = document.getElementById('new-barn-name')
     // this.horseForm = document.getElementById('new-horse-form')
@@ -55,6 +56,30 @@ class Horses {
 
     // Attempt 2: 
     for(let horse of this.horses) {
+      this.createGrid()
+      // this.photo.innerHTML = `<img src=${horse.photo}></img>`
+      // this.barnName.innerHTML = `Barn Name: ${horse.barn_name}`
+      // this.registeredName.innerHTML = `Registered Name: ${horse.registered_name}`
+      // this.sire.innerHTML = `Sire: ${horse.sire}`
+      // this.dam.innerHTML = `Dam: ${horse.dam}`
+      // this.pedigree.innerHTML = `Pedigree: ${horse.pedigree}`
+      // this.registration.innerHTML = `Registration: ${horse.registration}`
+      // this.color.innerHTML = `Color: ${horse.color}`
+      // this.patternType.innerHTML = `Pattern Type: ${horse.pattern_type}`
+      // this.tested.innerHTML = `Tested: ${horse.tested}`
+      // this.tobianoGene.innerHTML = `Tobiano Gene: ${horse.tobiano_gene}`
+      // // Also need to iterate over each offspring here
+      // this.offspring.innerHTML = `Offspring: <p>Year: ${horse.offspring.year}</p><p>Sex: ${horse.offspring.sex}</p><p>Sire: ${horse.offspring.sire}</p>`
+    }
+  }
+
+    createGrid() {
+      // APPEND id 'horses-content' TO id 'horses-container'
+      let horsesContent = document.getElementsByClassName('horses-content')
+
+      this.horsesContainer.append(horsesContent)
+      for(let horse of this.horses) {
+
       this.photo.innerHTML = `<img src=${horse.photo}></img>`
       this.barnName.innerHTML = `Barn Name: ${horse.barn_name}`
       this.registeredName.innerHTML = `Registered Name: ${horse.registered_name}`
@@ -67,14 +92,36 @@ class Horses {
       this.tested.innerHTML = `Tested: ${horse.tested}`
       this.tobianoGene.innerHTML = `Tobiano Gene: ${horse.tobiano_gene}`
       // Also need to iterate over each offspring here
-      this.offspring.innerHTML = `Offspring: <p>Year: ${horse.offspring.year}</p><p>Sex: ${horse.offspring.sex}</p><p>Sire: ${horse.offspring.sire}</p>`
-    }
+      // this.offsprings.innerHTML = `Offspring: <p>Year: ${horse.offsprings[0].year}</p>`
+      // <p>Sex: ${horse.offsprings.sex}</p><p>Sire: ${horse.offsprings.sire}</p>`
+      }
+      // FIX THIS
 
+
+      `<div id="horses-content">`
+
+      `<div class="grid">`
+        `<div class="span-col-2">`
+          `<p id="photo"></p>`
+          `<p id="barn-name"></p>`
+          `<p id="registered-name"></p>`
+        `</div>`
+        `<div id="sire"></div>`
+        `<div id="color"></div>`
+        `<div id="dam"></div>`
+        `<div id="pattern-type"></div>`
+        `<div id="pedigree"></div>`
+        `<div id="tested"></div>`
+        `<div id="registration"></div>`
+        `<div id="tobiano-gene"></div>`
+        `<div class="span-col-2" id="offsprings">Offspring: </div>`
+      `</div>`
+    }
 
     // this.photo.innerHTML = this.horses.map(horse => horse.photo)
     // barnName.innerHTML = this.horses.map(horse => `<p>Horse Name:  ${horse.barn_name}</p>`).join('')
     // registeredName.innerHTML = this.horses.map(horse => `<p>Registered Name: ${horse.registered_name}</p>`).join('')
-  }
+  
 
   horsesHTML() {
     return this.horses.map(horse => horse.render()).join('')
