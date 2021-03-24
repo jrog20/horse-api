d2dad0b8aad0384b8faf86742968507543d1734b
jrog20 <jaimerogstad@gmail.com>
2021-03-23 21:08:39 -0400

    Clean up file

--- frontend/src/adapters/HorsesAdapter.js
+++ frontend/src/adapters/HorsesAdapter.js
@@ -19,22 +19,8 @@ class HorsesAdapter {
     return fetch(this.baseUrl, horseCreateParams).then(res => res.json())
   }
 
-  // updateHorse(value, id, dbname) {
-  //   const horseUpdateParams = {
-  //     method: 'PATCH',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({ dbname: value })
-  //   }
-  //   return fetch(`${this.baseUrl}/${id}`, horseUpdateParams).then(res => res.json())
-  // }
-
   updateHorse(value, id, name) {
-    // let db_name = name
     let horse = {}
-      // db_name: value,
-    // }
     horse[name] = value
     return fetch(`${this.baseUrl}/${id}`, {
       method: 'PATCH',
