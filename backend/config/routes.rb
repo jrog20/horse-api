b70a4a588f7df5e24f63bf6c6813c2aeb5148451
jrog20 <jaimerogstad@gmail.com>
2021-03-27 19:07:32 -0400

    Add nested routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :offsprings
   namespace :api do
     namespace :v1 do
-      resources :horses
+      resources :horses do
+        resources :offsprings
+      end
     end
   end
 end
