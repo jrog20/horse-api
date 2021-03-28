0f99da6ac944b4f21495da66b80bd404d33053ac
jrog20 <jaimerogstad@gmail.com>
2021-03-28 14:16:50 -0400

    Clean up file

--- frontend/src/components/offspring.js
+++ frontend/src/components/offspring.js
@@ -5,21 +5,4 @@ class Offspring {
     this.sire = offspringJSON.sire
     this.id = offspringJSON.id
   }
-
-
-
-  // renderOffspringYear() {
-  //   console.log(this)
-  //   return `<p><strong>Year: </strong><span data-id=${this.id} data-field="year">${this.year}</span></p>`
-  // }
-
-  // renderOffspringSex() {
-  //   // console.log(this.sex)
-  //   return `<p><strong>Sex: </strong><span data-id=${this.id} data-field="year">${this.sex}</span></p>`
-  // }
-
-  // renderOffspringSire() {
-  //   // console.log(this.offspring)
-  //   return `<p><strong>Sire: </strong><span data-id=${this.id} data-field="year">${this.sire}</span></p>`
-  // }
 }
